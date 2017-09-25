0c652436864a65e9187e38398022497e31ba985d
jakubmironczuk123 <jakub.mironczuk@tlen.pl>
2017-09-25 15:36:57 +0200

    zadanie2

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string imie= "kuba";
+string nazwisko= "mironczuk";
+int rokurodzenia= 1997;
+int aktualnyrok= 2017;
+
+int main()
+{
+    cout<< "witam, nazywam sie"<<" "<< imie <<" "<< nazwisko<<" "<< "i mam"<< aktualnyrok - rokurodzenia<<" "<<"lat"<<endl;
+
+    return 0;
+}
